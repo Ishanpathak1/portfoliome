31087a5edb477dfbb0a864bc65d224a9813b7b07
Ishan Pathak <ishan.pathak2711@gmail.com>
2025-08-31 12:10:12 -0400

    Updated the sitemap.xml

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,28 +1,14 @@
 import { MetadataRoute } from 'next'
 
-// Helper function to get clean URL
-function getBaseUrl(): string {
-  const envUrl = process.env.NEXT_PUBLIC_APP_URL?.replace('NEXT_PUBLIC_APP_URL=', '').trim();
-  
-  if (envUrl && envUrl.startsWith('http')) {
-    return envUrl;
-  }
-  
-  return process.env.NODE_ENV === 'production' ? 'https://take-my.info' : 'http://localhost:3000';
-}
-
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = getBaseUrl();
-  
   return {
     rules: [
       {
         userAgent: '*',
-        allow: ['/', '/sitemap.xml'],
+        allow: ['/'],
         disallow: ['/api/'],
       },
     ],
-    sitemap: `${baseUrl}/sitemap.xml`,
-    host: baseUrl,
+    sitemap: 'https://take-my.info/sitemap.xml',
   }
-} 
+}
